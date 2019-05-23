4dc4195e9c59a1eff8b929c41c0592c6a07d3429
bing_huang <1278032416@qq.com>
2019-05-23 14:03:20 +0800

    cloud 链路追踪

---
+++ spring_cloud_zipkin/src/main/java/com/cn/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,19 @@
+package com.cn.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * 链路追踪
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
