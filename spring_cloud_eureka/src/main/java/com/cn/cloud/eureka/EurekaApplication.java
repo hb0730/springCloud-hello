fae06f5ace9aaee6fa5c3867de6f31728c2c3a08
bing_huang <1278032416@qq.com>
2019-05-23 14:01:17 +0800

    cloud 注册中心

---
+++ spring_cloud_eureka/src/main/java/com/cn/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.cn.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册于发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
